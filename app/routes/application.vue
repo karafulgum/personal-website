cdd240076edd1d0e8596c760dd082bd6efb1ee98
Kara Fulgum <karafulgum@gmail.com>
2016-11-22 12:47:48 -0600

    Nav HTML & CSS

---
+++ app/routes/application.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+    <div class="nav">
+      <div class="container">
+        <div class="nav__content">
+          <img src="../images/KLOGO-S.png" class="logo" alt="Kara Fulgum Logo">
+          <ul class="nav__right">
+            <li>
+              <router-link to='#' class="nav__right-item">About</router-link>
+            </li>
+            <li>
+              <router-link to='#' class="nav__right-item">Portfolio</router-link>
+            </li>
+            <li>
+              <router-link to='#' class="nav__right-item">Contact</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
